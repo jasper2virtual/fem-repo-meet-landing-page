ea9f49c3bc1218d2976fccfe99b8249968040a9b
Virtual Jasper <jasper2virtual@hotmail.com>
2024-11-06 18:17:01 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      screens:{
+        mobile:'365px',
+        tablet:'768px',
+        desktop:'1044px',
+      },
+      fontFamily:{
+        'redhatdisplay':['Red Hat Display', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
